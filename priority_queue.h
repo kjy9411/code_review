14e6c89096a16cb9e06e036010ec16069b0bd490
kjy9411 <kjy9411@samsung.com>
2019-10-23 14:07:47 +0900

    Update priority_queue.h - coding style fix
    C++ coding style fix

--- priority_queue.h
+++ priority_queue.h
@@ -1,15 +1,21 @@
+#ifndef PRIORITY_QUEUE_H_
+#define PRIORITY_QUEUE_H_
+
 #include <queue>
 
 template <typename T> 
 class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+  private:
+    std::priority_queue<T> storage_;
+  
+  public:
+    templatePriorityQueue();
+    ~templatePriorityQueue();
+    bool empty() const;
+    const T& top() const;
+    int size();
+    void push(const T&);
+    void pop();	
 };
+
+#endif  // PRIORITY_QUEUE_H_
