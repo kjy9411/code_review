3733563fef0e31a9874d75bc403a54eb00e275e9
kjy9411 <kjy9411@samsung.com>
2019-10-23 14:05:41 +0900

    Update priority_queue.cc - coding style fix
    C++ coding style fix

--- priority_queue.cc
+++ priority_queue.cc
@@ -1,34 +1,48 @@
+#include <gtest/gtest.h>
 #include "priority_queue.h"
 
-template <>
-templatePriorityQueue<int>::templatePriorityQueue(){
+TEST(PriorityQueueTest, Trivial) {
+  templatePriorityQueue<int> expTarget;
+
+  //Test 1 - Initalize
+  ASSERT_TRUE(expTarget.empty()); // empty test
+
+  //Test 2 - Push Datas and Top size
+  expTarget.push(1);
+  expTarget.push(4);
+  expTarget.push(2);
+  expTarget.push(3);
+  expTarget.push(8);
+  expTarget.push(5);
+
+  ASSERT_EQ(expTarget.size(), 6);
+  ASSERT_EQ(expTarget.top(), 8);
+  ASSERT_FALSE(expTarget.empty());
+
+  //Test 3 - pop test
+  for (int i = 0; i < 3; i++) {
+	  expTarget.pop();
+  }
+
+  ASSERT_EQ(expTarget.top(), 3);
+  ASSERT_EQ(expTarget.size(), 3);
+  ASSERT_FALSE(expTarget.empty());
+
+  for (int i = 0; i < 3; i++) {
+	  expTarget.pop();
+  }
+
+  ASSERT_TRUE(expTarget.empty());
+	
+  /* Fail Example - If you activate this code, Test will fail.
+   * expTarget.pop();
+   // non-Fatal Failure - not terminate this test case when result of empty method is false.
+   * EXPECT_TRUE(expTarget.empty());
+   * ASSERT_EQ(expTarget.size(),0);
+  */
 }
 
-template <>
-templatePriorityQueue<int>::~templatePriorityQueue(){
-}
-
-template <>
-bool templatePriorityQueue<int>::empty() const{
-        return storage.empty();
-}
-
-template <>
-int templatePriorityQueue<int>::size() {
-        return storage.size();
-}
-
-template <>
-const int& templatePriorityQueue<int>::top() const{
-        return storage.top();
-}
-
-template <>
-void templatePriorityQueue<int>::pop(){
-        storage.pop();
-}
-
-template <>
-void templatePriorityQueue<int>::push(const int& val){
-        storage.push(val);
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
